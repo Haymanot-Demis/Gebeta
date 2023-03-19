1a17b13825c1395b6f93cdcbd2f5428ff1e71a30
Haymanot-Demis <haymedin21@gmail.com>
2023-03-19 15:23:33 +0300

    Revert "add dish modal"
    This reverts commit 737967317b58b26f42846db21fa958520626de0f.

---
+++ Client/accounts/LoungeAdmin/js/modal.js
@@ -0,0 +1,28 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+const saveBtn = document.querySelector(".modal-btn.save-btn");
+const inputs = modal.querySelectorAll("input");
+const selects = modal.querySelectorAll("select");
+const desc = modal.querySelector("textarea");
+console.log(inputs);
+console.log(selects);
+console.log(desc);
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+openModalBtn.addEventListener("click", openModal);
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+closeModalBtn.addEventListener("click", closeModal);
+saveBtn.addEventListener("click", () => {
+  closeModal();
+});
+
+// overlay.addEventListener("click", closeModal);
