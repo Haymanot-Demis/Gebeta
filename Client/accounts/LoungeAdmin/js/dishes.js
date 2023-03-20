4973135b7afea119c5dc1503d9acfc6b2df51bf1
Haymanot-Demis <haymedin21@gmail.com>
2023-03-20 12:28:18 +0300

    edit dish modal

--- Client/accounts/LoungeAdmin/js/dishes.js
+++ Client/accounts/LoungeAdmin/js/dishes.js
@@ -1,113 +1,158 @@
 import { deleteBtnAction, editBtnAction } from "./eventListners.js";
 import {
-  createCustomElement,
-  attachEventListner,
-  rowEventListner,
+	createCustomElement,
+	attachEventListner,
+	rowEventListner,
 } from "./helpers.js";
 import {
-  dashboardContainer,
-  lists,
-  rows,
-  details,
-  tabularData,
-  table,
-  recentCustomers,
+	dashboardContainer,
+	lists,
+	rows,
+	details,
+	tabularData,
+	table,
+	recentCustomers,
 } from "./common-elements.js";
+import {
+	openModal,
+	update,
+	saveBtn,
+	modal,
+	overlay,
+	desc,
+	inputs,
+	selects,
+	flag,
+} from "./modal.js";
+
+var deleteBtns;
+var editBtns;
 
 try {
-  var response;
-  lists[2].classList.add("hovered");
-  response = await axios.get("http://localhost:3000/dishes");
-  const dishes = response.data;
-  tabularData.querySelector(".title").innerText = "Dishes";
-  tabularData.querySelector("table").innerHTML = "";
+	var response;
+	lists[2].classList.add("hovered");
+	response = await axios.get("http://localhost:3000/dishes");
+	const dishes = response.data;
+	tabularData.querySelector(".title").innerText = "Dishes";
+	tabularData.querySelector("table").innerHTML = "";
 
-  let thead = createCustomElement("thead");
-  let tr = createCustomElement("tr");
-  let td = createCustomElement("td", { innerText: "#" });
-  let td1 = createCustomElement("td", { innerText: "Name" });
-  let td2 = createCustomElement("td", { innerText: "Price" });
-  let td3 = createCustomElement("td", { innerText: "Description" });
-  let td4 = createCustomElement("td", { innerText: "Category" });
-  // let td5 = createCustomElement("td", { innerText: "Quantity" });
-  let td6 = createCustomElement("td", { innerText: "Image" });
-  let td7 = createCustomElement("td", { innerText: "Lounge" });
+	let thead = createCustomElement("thead");
+	let tr = createCustomElement("tr");
+	let td = createCustomElement("td", { innerText: "#" });
+	let td1 = createCustomElement("td", { innerText: "Name" });
+	let td2 = createCustomElement("td", { innerText: "Price" });
+	let td3 = createCustomElement("td", { innerText: "Description" });
+	let td4 = createCustomElement("td", { innerText: "Category" });
+	// let td5 = createCustomElement("td", { innerText: "Quantity" });
+	let td6 = createCustomElement("td", { innerText: "Image" });
+	let td7 = createCustomElement("td", { innerText: "Lounge" });
 
-  tr.append(td, td1, td2, td3, td4, td6, td7);
-  thead.appendChild(tr);
-  tabularData.querySelector("table").append(thead);
-  tabularData.querySelector("table").append(createCustomElement("tbody"));
-  let tbody = tabularData.querySelector("table").querySelector("tbody");
+	tr.append(td, td1, td2, td3, td4, td6, td7);
+	thead.appendChild(tr);
+	tabularData.querySelector("table").append(thead);
+	tabularData.querySelector("table").append(createCustomElement("tbody"));
+	let tbody = tabularData.querySelector("table").querySelector("tbody");
 
-  let i = 1;
-  for (let dish of dishes) {
-    tr = createCustomElement("tr", {
-      id: dish._id,
-      title: "Double Click to edit this dish",
-    });
+	let i = 1;
+	for (let dish of dishes) {
+		tr = createCustomElement("tr", {
+			id: dish._id,
+			title: "Double Click to edit this dish",
+		});
 
-    td = createCustomElement("td", { innerText: i });
-    td1 = createCustomElement("td", { innerText: dish.name });
-    td2 = createCustomElement("td", { innerText: dish.price });
-    td3 = createCustomElement("td", {
-      innerText: dish.description.split(" ").slice(0, 10).join(" "),
-    });
-    td4 = createCustomElement("td", { innerText: dish.category });
-    td6 = createCustomElement("td");
+		td = createCustomElement("td", { innerText: i });
+		td1 = createCustomElement("td", { innerText: dish.name });
+		td2 = createCustomElement("td", { innerText: dish.price });
+		td3 = createCustomElement("td", {
+			innerText: dish.description.split(" ").slice(0, 10).join(" "),
+		});
+		td4 = createCustomElement("td", { innerText: dish.category });
+		td6 = createCustomElement("td");
 
-    let img = createCustomElement("img", {
-      src: dish.image,
-      alt: dish.name + "image",
-      height: 130,
-      width: 200,
-    });
+		let img = createCustomElement("img", {
+			src: dish.image,
+			alt: dish.name + "image",
+			height: 130,
+			width: 200,
+		});
 
-    let deleteBtn = createCustomElement("button", {
-      innerText: "Delete",
-      id: "E" + dish._id,
-      class: "delte-btn",
-    });
-    deleteBtn.classList.add("delete-btn");
-    deleteBtn.classList.add("button-9");
-    deleteBtn.setAttribute("role", "button");
-    let editBtn = createCustomElement("button", {
-      innerText: "Edit",
-      id: "D" + dish._id,
-      class: "edit-btn",
-    });
-    editBtn.classList.add("edit-btn");
-    editBtn.classList.add("button-9");
-    editBtn.setAttribute("role", "button");
-    let div = createCustomElement("div", {
-      id: "C" + dish._id,
-      class: "btn-div",
-      style: "display:none;justify-content:space-evenly",
-    });
-    div.classList.add("btn-div");
-    div.append(deleteBtn, editBtn);
-    td6.append(img, div);
+		let deleteBtn = createCustomElement("button", {
+			innerText: "Delete",
+			id: "E" + dish._id,
+			class: "delete-btn",
+		});
+		deleteBtn.classList.add("delete-btn");
+		deleteBtn.classList.add("button-9");
+		deleteBtn.setAttribute("role", "button");
+		let editBtn = createCustomElement("button", {
+			innerText: "Edit",
+			id: "D" + dish._id,
+			class: "edit-btn",
+		});
+		editBtn.classList.add("edit-btn");
+		editBtn.classList.add("button-9");
+		editBtn.setAttribute("role", "button");
+		let div = createCustomElement("div", {
+			id: "C" + dish._id,
+			class: "btn-div",
+			style: "display:none;justify-content:space-evenly",
+		});
+		div.classList.add("btn-div");
+		div.append(deleteBtn, editBtn);
+		td6.append(img, div);
 
-    td7 = createCustomElement("td", { innerText: dish.lounge?.name });
-    tr.append(td, td1, td2, td3, td4, td6, td7);
-    tbody.append(tr);
-    i++;
-  }
+		td7 = createCustomElement("td", { innerText: dish.lounge?.name });
+		tr.append(td, td1, td2, td3, td4, td6, td7);
+		tbody.append(tr);
+		i++;
+	}
 
-  if (details.lastElementChild == recentCustomers) {
-    details.removeChild(details.lastElementChild);
-  }
+	if (details.lastElementChild == recentCustomers) {
+		details.removeChild(details.lastElementChild);
+	}
 
-  details.style.gridTemplateColumns = "1fr";
-  details.style.padding = "50px";
-  let rows = tabularData.querySelector("table").querySelectorAll("tr");
+	details.style.gridTemplateColumns = "1fr";
+	details.style.padding = "50px";
+	let rows = tabularData.querySelector("table").querySelectorAll("tr");
 
-  const deleteBtns = document.querySelectorAll(".delete-btn");
-  const editBtns = document.querySelectorAll(".edit-btn");
-  attachEventListner(deleteBtns, "click", deleteBtnAction, deleteBtns);
-  attachEventListner(editBtns, "click", editBtnAction);
-  let url =
-    "http://127.0.0.1:5500/Client/accounts/LoungeAdmin/editDish.html?id=";
-  rowEventListner(rows, url);
+	deleteBtns = document.querySelectorAll(".delete-btn");
+	editBtns = document.querySelectorAll(".edit-btn");
+	//attaching event listeners to the edit buttons
+	for (let i = 0; i < editBtns.length; i++) {
+		editBtns[i].addEventListener("click", (event) => {
+			edittingActions(dishes[i], inputs, selects, desc, event);
+		});
+		rows[i + 1].addEventListener("dblclick", (event) => {
+			edittingActions(dishes[i], inputs, selects, desc, event);
+		});
+	}
+
+	attachEventListner(deleteBtns, "click", deleteBtnAction);
+	// attachEventListner(editBtns, "click", editBtnAction);
+	// attachEventListner(rows, "dblclick", editBtnAction);
+
+	rowEventListner(rows);
 } catch (error) {
-  console.log(error);
+	console.log(error);
 }
+
+function edittingActions(dish, inputs, selects, desc, event) {
+	console.log(dish);
+	openModal(event);
+	flag.innerText = "Editing";
+	saveBtn.id = dish._id;
+	inputs[0].value = dish.name;
+	inputs[1].parentNode.nextElementSibling
+		.querySelector("#imagePreview")
+		.querySelector("img").src = dish.image;
+
+	console.log(dish.image);
+	inputs[2].checked = dish.isfasting;
+	inputs[3].checked = !dish.isfasting;
+	inputs[4].value = dish.price;
+	console.log(inputs[4]);
+	desc.innerText = dish.description;
+	update();
+}
+
+export { deleteBtns, editBtns };
--- Client/accounts/LoungeAdmin/js/modal.js
+++ Client/accounts/LoungeAdmin/js/modal.js
@@ -6,75 +6,158 @@ const saveBtn = document.querySelector(".modal-btn.save-btn");
 const inputs = modal.querySelectorAll("input");
 const selects = modal.querySelectorAll("select");
 const desc = modal.querySelector("textarea");
+const flag = modal.querySelector(".flex").firstElementChild;
+var formdata;
+var error = false;
+
 import { validate } from "./helpers.js";
 import escapeHTML from "../../../script/escapeHTML.js";
 import { DISHES_URL } from "../../../config/EndPoints.js";
 
-const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+const openModal = function (event) {
+	// console.log(event.clientY);
+	// console.log(event.clientX);
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden");
+	// modal.style.top = event.clientY - 50 + "px";
+	// modal.style.left = event.clientX - 50 + "px";
 };
 
-openModalBtn.addEventListener("click", openModal);
+openModalBtn.addEventListener("click", (event) => {
+	openModal(event);
+	flag.innerHTML = "Adding";
+	saveBtn.addEventListener("click", async () => {
+		if (!error) {
+			const response = await axios.post(DISHES_URL, formdata);
+		}
+	});
+});
 
 const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+	modal.querySelector("form").reset();
+	inputs[1].parentNode.nextElementSibling
+		.querySelector("#imagePreview")
+		.querySelector("img").src = "../../images/camera.png";
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
 };
 
 closeModalBtn.addEventListener("click", closeModal);
 
-saveBtn.addEventListener("click", async () => {
-  let error = false;
+saveBtn.addEventListener("click", save);
 
-  for (let input of inputs) {
-    if (validate(escapeHTML(input.value)) == true) {
-      input.parentElement?.classList.add("success");
-    } else if (validate(escapeHTML(input.value)) == "required") {
-      input.parentElement?.classList.add("error");
-      input.parentNode.querySelector("small").innerText =
-        "This field is required";
-      error = true;
-    } else if (validate(escapeHTML(input.value)) == "invalid") {
-      input.parentElement?.classList.add("error");
-      input.parentNode.querySelector("small").innerText =
-        "Please conform to the field pattern";
-      error = true;
-    }
-  }
-
-  for (let select of selects) {
-    if (validate(escapeHTML(select.value)) == true) {
-      select.parentElement?.classList.add("success");
-    } else if (validate(escapeHTML(select.value)) == "required") {
-      select.parentElement?.classList.add("error");
-      select.parentNode.querySelector("small").innerText =
-        "This field is required";
-      error = true;
-    } else if (validate(escapeHTML(select.value)) == "invalid") {
-      select.parentElement?.classList.add("error");
-      select.parentNode.querySelector("small").innerText =
-        "Please conform to the field pattern";
-      error = true;
-    }
-  }
-
-  if (!error) {
-    try {
-      const formdata = new FormData();
-      formdata.append("name", escapeHTML(inputs[0].value));
-      formdata.append("image", inputs[1]?.files[0]);
-      formdata.append("price", escapeHTML(inputs[4].value));
-      formdata.append("type", escapeHTML(selects[0].value));
-      formdata.append("lounge", escapeHTML(selects[1].value));
-      formdata.append("category", escapeHTML(selects[2].value));
-      formdata.append("description", escapeHTML(desc.value));
-      const response = await axios.post(DISHES_URL, formdata);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  closeModal();
+// previewing image before upload
+inputs[1].addEventListener("change", () => {
+	inputs[1].parentNode.nextElementSibling
+		.querySelector("#imagePreview")
+		.querySelector("img").src = URL.createObjectURL(inputs[1].files[0]);
+	inputs[1].onload = () => {
+		URL.revokeObjectURL(URL.createObjectURL(inputs[1].files[0]));
+	};
 });
+
+async function save() {
+	formdata = new FormData();
+	error = false;
+	for (let i = 0; i < inputs.length; i++) {
+		if (i == 1) {
+			continue;
+		}
+		if (validate(escapeHTML(inputs[i].value ?? "")) == true) {
+			inputs[i].parentElement?.classList.add("success");
+			inputs[i].parentElement?.classList.remove("error");
+		} else if (validate(escapeHTML(inputs[i].value ?? "")) == "required") {
+			inputs[i].parentElement?.classList.add("error");
+			inputs[i].parentNode.querySelector("small").innerText =
+				"This field is required";
+			error = true;
+		} else if (validate(escapeHTML(inputs[i].value ?? "")) == "invalid") {
+			inputs[i].parentElement?.classList.add("error");
+			inputs[i].parentNode.querySelector("small").innerText =
+				"Please conform to the field pattern";
+			error = true;
+		}
+	}
+
+	let file = inputs[1].files[0];
+	if (file) {
+		console.log(file);
+		let extname = file.type.split("/")[1];
+		console.log(extname);
+		const allowed = ["png", "jpg", "jpeg"];
+		console.log(allowed.indexOf(extname));
+		if (allowed.indexOf(extname) == -1) {
+			inputs[1].parentElement.classList.add("error");
+			inputs[1].parentNode.parentNode.parentNode.parentNode.querySelector(
+				"small"
+			).innerText = "Invalid image file format";
+			error = true;
+		}
+	}
+	if (!file && flag.innerText == "Adding") {
+		console.log(inputs[1].parentNode.parentNode.parentNode.parentNode);
+		inputs[1].parentNode.parentNode.parentNode.parentNode.classList.add(
+			"error"
+		);
+		inputs[1].parentNode.parentNode.parentNode.parentNode.querySelector(
+			"small"
+		).innerText = "Please upload an image";
+		error = true;
+	}
+
+	for (let select of selects) {
+		if (validate(escapeHTML(select.value)) == true) {
+			select.parentElement?.classList.add("success");
+		} else if (validate(escapeHTML(select.value)) == "required") {
+			select.parentElement?.classList.add("error");
+			select.parentNode.querySelector("small").innerText =
+				"This field is required";
+			error = true;
+		} else if (validate(escapeHTML(select.value)) == "invalid") {
+			select.parentElement?.classList.add("error");
+			select.parentNode.querySelector("small").innerText =
+				"Please conform to the field pattern";
+			error = true;
+		}
+	}
+
+	if (!error) {
+		formdata.append("name", escapeHTML(inputs[0].value));
+		formdata.append("image", inputs[1]?.files[0]);
+		formdata.append("price", escapeHTML(inputs[4].value));
+		formdata.append("type", escapeHTML(selects[0].value));
+		formdata.append("lounge", escapeHTML(selects[1].value));
+		formdata.append("category", escapeHTML(selects[2].value));
+		formdata.append("description", escapeHTML(desc.value));
+		// closeModal();
+	} else {
+		console.log("no form data");
+	}
+}
+
+async function update() {
+	console.log("update");
+	saveBtn.addEventListener("click", async () => {
+		console.log("clicked");
+		if (!error) {
+			const response = await axios.put(DISHES_URL + "/" + saveBtn.id, formdata);
+			closeModal();
+		} else {
+			console.log(error);
+		}
+	});
+}
+
+export {
+	openModal,
+	update,
+	saveBtn,
+	modal,
+	overlay,
+	inputs,
+	selects,
+	desc,
+	flag,
+};
 
 // overlay.addEventListener("click", closeModal)
